3372af89e41fc85169941c85640813fce0f1b191
Ashutosh6226Tiwari <228049884+Ashutosh6226Tiwari@users.noreply.github.com>
2026-09-22 00:52:55 +0530

    Add Easy Python Solution for 268. Missing Number from LeetCode

--- LeetCode/Python/Easy/268. Missing Number/Solution.py
+++ LeetCode/Python/Easy/268. Missing Number/Solution.py
@@ -1,12 +1,18 @@
 class Solution:
     def missingNumber(self, nums: list[int]) -> int:
         n=len(nums)
-        nums.sort()
-        for i in range (n):
-            if i !=nums[i]:
-                return i
-        return n
+        total= (n*(n+1))//2
+        curr_sum=sum(nums)
+        return total - curr_sum
+        
+        
+        
+        # n=len(nums)
+        # nums.sort()
+        # for i in range (n):
+        #     if i !=nums[i]:
+        #         return i
+        # return n
 
 
-    
         
