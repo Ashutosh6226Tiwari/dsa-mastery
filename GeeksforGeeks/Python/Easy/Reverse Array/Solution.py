0da8e144fdd3284f652f7aa45f80c57d113345b5
Ashutosh6226Tiwari <228049884+Ashutosh6226Tiwari@users.noreply.github.com>
2026-09-23 15:02:18 +0530

    Add Easy Python Solution for Reverse Array from GeeksforGeeks

---
+++ GeeksforGeeks/Python/Easy/Reverse Array/Solution.py
@@ -0,0 +1,13 @@
+class Solution:
+    def reverseArray(self, arr):
+        left,right=0 ,len(arr)-1
+        while left<right :
+            arr[left],arr[right]=arr[right],arr[left]
+            left+=1
+            right-=1
+        return arr
+        
+        # arr.reverse()
+        # return arr
+        
+    
