4fb1c09c9fbf677507af7d4010b0ec96d407dc13
Ashutosh6226Tiwari <228049884+Ashutosh6226Tiwari@users.noreply.github.com>
2026-09-25 00:37:55 +0530

    Add Easy Python Solution for Bubble Sort from GeeksforGeeks

--- GeeksforGeeks/Python/Easy/Bubble Sort/Solution.py
+++ GeeksforGeeks/Python/Easy/Bubble Sort/Solution.py
@@ -1,13 +1,23 @@
 class Solution:
     def bubbleSort(self,arr):
-        # for i in range (0,len(arr)-1):
-        #     if (arr[i]>arr[i+1]):
-        #         arr[i],arr[i+1]=arr[i+1],arr[i]
-    
-        # return arr
+        # n=len(arr)
+        # for counter in range (1,n):
+        #     for i in range (0,n-1):
+        #         if (arr[i]>arr[i+1]):
+        #             arr[i],arr[i+1]=arr[i+1],arr[i]
         
-        for i in range (1, len(arr)):
-            if (arr[i]<arr[i-1]):
-                arr[i],arr[i-1]=arr[i-1],arr[i]
+        #     return arr
+        
+        n=len(arr)
+        for i in range (1, n):
+            is_swap=False
+            for j in range (1,n):
+                if (arr[j]<arr[j-1]):
+                    arr[j],arr[j-1]=arr[j-1],arr[j]
+                    is_swap=True
+                    
+            if not is_swap :
+                break
         
             
+    
