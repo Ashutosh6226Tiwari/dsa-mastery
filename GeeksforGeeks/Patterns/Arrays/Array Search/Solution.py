0b66921928fb016418bc0a91db6e0c043a4c4172
Ashutosh6226Tiwari <228049884+Ashutosh6226Tiwari@users.noreply.github.com>
2026-09-25 00:31:09 +0530

    Add Basic Python Solution for Array Search from GeeksforGeeks

--- GeeksforGeeks/Patterns/Arrays/Array Search/Solution.py
+++ GeeksforGeeks/Patterns/Arrays/Array Search/Solution.py
@@ -2,4 +2,8 @@ class Solution:
     def search(self, arr, x):
         for i in range (0,len(arr)):
             if arr[i]==x:
-                return i
+                index =i
+                break
+            else :
+                index=-1
+        return index
