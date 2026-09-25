de2a8a72ded61454d5702372c927906270029d08
Ashutosh6226Tiwari <228049884+Ashutosh6226Tiwari@users.noreply.github.com>
2026-09-25 23:41:02 +0530

    Add Easy Python Solution for Number of Occurrence from GeeksforGeeks

---
+++ GeeksforGeeks/Patterns/Arrays/Number of Occurrence/Solution.py
@@ -0,0 +1,7 @@
+class Solution:
+    def countFreq(self, arr, target):
+        count =0
+        for i in range (0,len(arr)):
+            if arr[i]==target:
+                count+=1
+        return count
