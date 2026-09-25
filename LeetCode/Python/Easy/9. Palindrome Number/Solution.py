39691923712a5ec0d4b6142fb9732ef54ad70c5e
Ashutosh6226Tiwari <228049884+Ashutosh6226Tiwari@users.noreply.github.com>
2026-09-26 00:33:55 +0530

    Add Easy Python Solution for 9. Palindrome Number from LeetCode

--- LeetCode/Python/Easy/9. Palindrome Number/Solution.py
+++ LeetCode/Python/Easy/9. Palindrome Number/Solution.py
@@ -1,15 +1,27 @@
 class Solution:
     def isPalindrome(self, x: int) -> bool:
-        s=str(x)
-        left =0
-        right= len(s)-1
-        while left < right :
-            if s[left]!=s[right]:
-                return False 
-            left+=1
-            right-=1
-        return True
-        
+        if x<0 :
+            return False 
+        original= x
+        reversed_num=0
+        while x>0:
+            n=x%10
+            reversed_num=reversed_num*10+n
+            x=x//10
+        return reversed_num==original
+
+
+# method->2
+        # s=str(x)
+        # left =0
+        # right= len(s)-1
+        # while left < right :
+        #     if s[left]!=s[right]:
+        #         return False 
+        #     left+=1
+        #     right-=1
+        # return True
+# method->3
         # n=str(x)
         # if (n[::-1]==n[::]):
         #     return True
