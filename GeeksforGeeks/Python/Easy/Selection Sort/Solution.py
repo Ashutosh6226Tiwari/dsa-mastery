5bfdcf9000eca681b780823756edc988bc23ae5a
Ashutosh6226Tiwari <228049884+Ashutosh6226Tiwari@users.noreply.github.com>
2026-09-26 03:24:56 +0530

    Add Easy Python Solution for Selection Sort from GeeksforGeeks

--- GeeksforGeeks/Python/Easy/Selection Sort/Solution.py
+++ GeeksforGeeks/Python/Easy/Selection Sort/Solution.py
@@ -1,12 +1,13 @@
 class Solution: 
     def selectionSort(self, arr):
         n=len(arr)
-        for counter in range (1,n):
-            is_sorted = False
-            for j in range (1,n):
-                if arr[j]<arr[j-1]:
-                    arr[j],arr[j-1]=arr[j-1],arr[j]
-                    is_sorted = True 
-            if not is_sorted :
-                break
-        
+        for i in range (n-1):
+            min_idx=i
+            
+            for j in range (i+1,n):
+                if arr[j]<arr[min_idx]:
+                    min_idx=j
+            
+            if min_idx!=i:
+                arr[i],arr[min_idx]=arr[min_idx],arr[i]
+        return arr
